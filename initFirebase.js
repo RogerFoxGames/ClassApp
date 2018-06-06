1550d7edd4c9e0f3808dd758695d6d6ff4fe7ae9
RogerFoxGames <rogerfoxgames@gmail.com>
2018-06-06 17:35:02 +0200

    inizializzato firebase

---
+++ initFirebase.js
@@ -0,0 +1,10 @@
+//Inizializzo firebase
+var config = {
+  apiKey: "AIzaSyB3_qdaowx2rOxLCsB2gj2JVEeVHxFzyNI",
+  authDomain: "classapp-72611.firebaseapp.com",
+  databaseURL: "https://classapp-72611.firebaseio.com",
+  projectId: "classapp-72611",
+  storageBucket: "classapp-72611.appspot.com",
+  messagingSenderId: "814235220128"
+};
+firebase.initializeApp(config);
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var app = angular.module("ClassApp", [])
 
+//controller di esempio
 app.controller("primoController",function($scope){
     $scope.nome = "Luca"
 })
